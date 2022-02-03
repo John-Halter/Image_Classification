203807fbd14814eb3bb8236f8c0d7d5c5b43a4e9
John-Halter <johnnyhalter17@gmail.com>
2022-02-03 11:50:30 -0700

    Initial CNN

--- CNN.py
+++ CNN.py
@@ -5,6 +5,8 @@ import matplotlib
 import numpy as np
 import keras
 import tensorflow as tf
+from keras.models import Sequential
+
 from Constants import LIST_OF_SPECIES, output_dir
 from dataset_manipulation import create_training_data
 matplotlib.use('TkAgg')
@@ -18,8 +20,64 @@ training_dataset = create_training_data(batch_size,img_height,img_width, output_
 testing_dataset = create_training_data(batch_size,img_height,img_width, output_dir, 'validation')
 
 class_names = training_dataset.class_names
-print(class_names)
 
 
+AUTOTUNE = tf.data.AUTOTUNE
 
+train_ds = training_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = testing_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+normalized_ds = training_dataset.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+
+num_classes = len(class_names)
+
+
+model = Sequential([
+  tf.keras.layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+print(model.summary())
+
+epochs=15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
 plt.show()
