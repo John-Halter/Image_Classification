6b7a55e36e069cf76a8a4325fcb1106531601f66
John-Halter <johnnyhalter17@gmail.com>
2022-02-21 11:57:15 -0700

    Changes made 02212022

--- data_gather.py
+++
@@ -1,7 +0,0 @@
-from bing_image_downloader import downloader
-
-def download_pictures(ls_of_names,num_of_pics, output_directory):
-    for i in ls_of_names:
-        downloader.download(query=f"{i} Butterfly",limit=num_of_pics,force_replace=True,output_dir=output_directory)
-
-    print("Printed All pictures successfully")
